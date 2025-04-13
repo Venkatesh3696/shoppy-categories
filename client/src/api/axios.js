28ac66b329fd5ef7dba47ccecbf231f9c57beea9
Venkatesh3696 <venkateshpentakota888@gmail.com>
2025-04-13 11:58:08 +0530

    add live backend url

--- client/src/api/axios.js
+++ client/src/api/axios.js
@@ -1,14 +1,6 @@
 import axios from "axios";
-import dotenv from "dotenv";
 
-dotenv.config();
-
-const url =
-  process.env.REACT_APP_ENV && process.env.REACT_APP_ENV.trim() === "production"
-    ? "https://shoppy-categories.onrender.com/"
-    : "http://localhost:5000/";
-
-console.log(url, "base url ");
+const url = "https://shoppy-categories.onrender.com/";
 
 export const API = axios.create({
   baseURL: url,
