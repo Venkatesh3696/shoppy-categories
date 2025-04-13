d6067f3c6d08b3eeb5374ff1b1b27968d0252ba4
Venkatesh3696 <venkateshpentakota888@gmail.com>
2025-04-13 11:23:04 +0530

    add dotenv

--- client/src/api/axios.js
+++ client/src/api/axios.js
@@ -1,9 +1,15 @@
 import axios from "axios";
+import dotenv from "dotenv";
+
+dotenv.config();
 
 const url =
-  process.env.REACT_APP_ENV === "production"
-    ? "https://shoppy-categories.onrender.com"
+  process.env.REACT_APP_ENV && process.env.REACT_APP_ENV.trim() === "production"
+    ? "https://shoppy-categories.onrender.com/"
     : "http://localhost:5000/";
+
+console.log(url, "base url ");
+
 export const API = axios.create({
   baseURL: url,
   withCredentials: true,
